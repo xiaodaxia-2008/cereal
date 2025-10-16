e18e9ac069caa5fcca0abb5bf67252398b035be2
xiaodaxia <xiaozisheng2008@hotmail.com>
2025-10-16 19:18:57 +0800

    test if FOR_EACH is defined

--- include/cereal/details/loop_macros.hpp
+++ include/cereal/details/loop_macros.hpp
@@ -5,11 +5,13 @@
 #define _EXPAND2(...) _EXPAND1(_EXPAND1(_EXPAND1(_EXPAND1(__VA_ARGS__))))
 #define _EXPAND1(...) __VA_ARGS__
 
+#ifndef FOR_EACH
 #define FOR_EACH(macro, ...) \
     __VA_OPT__(_EXPAND(_FOR_EACH_HELPER(macro, __VA_ARGS__)))
 #define _FOR_EACH_HELPER(macro, a1, ...) \
     macro(a1) __VA_OPT__(, _FOR_EACH_AGAIN _PARENS(macro, __VA_ARGS__))
 #define _FOR_EACH_AGAIN() _FOR_EACH_HELPER
+#endif
 
 #define FOR_EACH2(macro, arg0, ...) \
     __VA_OPT__(_EXPAND(_FOR_EACH_HELPER2(macro, arg0, __VA_ARGS__)))
